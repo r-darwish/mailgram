b9ce6f2b422314cd918f6853d71c8bb25692695e
Roey Darwish Dror <roey.ghost@gmail.com>
2018-02-09 11:11:35 +0200

    Add testing script

---
+++ tests/testing.py
@@ -0,0 +1,25 @@
+import sys
+import socket
+
+def main():
+    sock = socket.socket()
+    sock.connect((sys.argv[1], int(sys.argv[2])))
+    sock.sendall("""EHLO testing\r
+MAIL FROM:<from@somewhere.com>\r
+RCPT TO:<to@somewhere.com>\rq
+DATA\r
+Date: Wed, 07 Feb 2018 21:38:35 +0200\r
+From: from@somewhere.com\r
+To: to@somewhere.com\r
+Subject: Test Notification\r
+Priority: normal\r
+MIME-Version: 1.1\r
+Content-Type: text/plain; charset=us-ascii\r
+\r
+Success! You have properly configured your email notification settings\r
+.\r
+QUIT\r
+""".encode("ASCII"))
+
+if __name__ == '__main__':
+    sys.exit(main())
